70fca3b091bff271b16fec8dfc784ead298439ef
Olumide Koyenikan <olumide.koyenikan@yahoo.com>
2023-08-16 20:46:13 +0100

    created apis

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var express = require('express');
 var bodyParser = require('body-parser')
+const {getFromStore, getAllFromStore, addToStore, updateStore, deleteFromStore, deleteAllFromStore} = require('./storage.js')
 var apiConfig = express();
 var cors = require('cors');
 apiConfig.use(cors());
@@ -10,26 +11,77 @@ apiConfig.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLim
 
 var store = {};
 
-apiConfig.post('/get', async(req, res)=>{
-    const {z} = req.body;
+apiConfig.get('/getAll', async(req, res)=>{
+    //const {z} = req.body;
+    var items = getAllFromStore()
+    if (items) {
+      res.json(items);
+    } else {
+      res.status(404).json({ message: 'Items not found' });
+    }
+})
+
+apiConfig.get('/get/:id', async(req, res)=>{
+    //const {z} = req.body;
+    const itemId = parseInt(req.params.id);
+
+    const item = getFromStore(itemId)
+
+    if (item) {
+        res.json(item);
+      } else {
+        res.status(404).json({ message: 'Item not found' });
+      }
 
 })
    
 
 apiConfig.post('/post', async(req, res)=>{
-    const {z} = req.body;
+    //const {z} = req.body;
+    const newItem = req.body;
+    const resp = addToStore(newItem)
+    res.status(201).json(resp);
+})
+
+
+apiConfig.put('/update/:id', async(req, res)=>{
+    //const {z} = req.body;
+    const itemId = parseInt(req.params.id);
+    const updatedItem = req.body;
+    var found = updateStore(itemId, updatedItem)
+
+  if (found) {
+    res.json(found);
+  } else {
+    res.status(404).json({ message: 'Item not found' });
+  }
+
 
 })
 
 
-apiConfig.post('/update', async(req, res)=>{
-    const {z} = req.body;
+apiConfig.delete('/delete/:id', async(req, res)=>{
+    //const {z} = req.body;
+    const itemId = parseInt(req.params.id);
+    var found = deleteFromStore(itemId)
+
+  if (found) {
+    res.json();
+  } else {
+    res.status(404).json({ message: 'Item not found' });
+  }
 
 })
 
+apiConfig.delete('/deleteAll', async(req, res)=>{
+  //const {z} = req.body;
+  deleteAllFromStore()
 
-apiConfig.post('/delete', async(req, res)=>{
-    const {z} = req.body;
+  res.json();
+
 
 })
 
+http.Server(apiConfig).listen(3002, ()=>{
+    console.log("server running on 3002")
+})
